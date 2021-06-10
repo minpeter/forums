3643b53af95c841cf97b51fa413c58903d97a980
minpeter <kali2005611@gmail.com>
2021-06-11 08:26:29 +0900

    댓글

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,6 +1,5 @@
-from flask import Blueprint, render_template
-
-from pybo.models import Question
+from flask import Blueprint, url_for
+from werkzeug.utils import redirect
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
@@ -9,8 +8,6 @@ bp = Blueprint('main', __name__, url_prefix='/')
 def hello_pybo():
     return 'Hello, Pybo!'
 
-
 @bp.route('/')
 def index():
-    question_list = Question.query.order_by(Question.create_date.desc())
-    return render_template('question/question_list.html', question_list=question_list)
+    return redirect(url_for('question._list'))
